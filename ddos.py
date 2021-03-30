1296cf326a9a013f3fdd05b40106d28115eef733
krishpranav <krisna.pranav@gmail.com>
2021-03-30 09:50:28 +0530

    done changes

--- ddos.py
+++ ddos.py
@@ -7,4 +7,10 @@ import time
 import socket
 import random
 #code time
-
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
